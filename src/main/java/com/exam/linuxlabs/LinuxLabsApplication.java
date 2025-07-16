1648f67b2716dd2851478e43a689550a956066e4
namung08 <namung08@gmail.com>
2025-07-16 15:09:54 +0900

    :tada: feat: 리눅스 서버 테스트용

---
+++ src/main/java/com/exam/linuxlabs/LinuxLabsApplication.java
@@ -0,0 +1,13 @@
+package com.exam.linuxlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxLabsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LinuxLabsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/exam/linuxlabs/LinuxLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.linuxlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxLabsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
