22a5562ee2baa85d18f714e793f3edcf8f6e2db7
namung08 <namung08@gmail.com>
2025-07-17 15:48:06 +0900

    :sparkles: feat: cors 설정

--- src/main/java/com/exam/linuxlabs/controller/ApiController.java
+++ src/main/java/com/exam/linuxlabs/controller/ApiController.java
@@ -4,12 +4,10 @@ import com.exam.linuxlabs.dto.GreetingResponse;
 import com.exam.linuxlabs.service.GreetingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class ApiController {
